fb17690c4b857ba57f14ee961a127be27c078b94
Tosin Olaosebikan <tosinolaosebikan@Tosins-MacBook-Pro.local>
2024-09-26 01:12:45 +0000

    adds classname merge to the text component

--- packages/ui/text/index.tsx
+++ packages/ui/text/index.tsx
@@ -1,6 +1,32 @@
-import { Text as ReactNativeText } from "react-native";
+import React from "react";
+import {  Text as ReactNativeText, TextProps as RNTextProps } from "react-native";
 import { cssInterop } from "nativewind";
+import { cn } from "ui/utils/cn";
 
-export const Text = cssInterop(ReactNativeText, {
+interface TextProps extends RNTextProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+cssInterop(ReactNativeText, {
   className: "style",
 });
+
+export const Text = React.forwardRef<React.ElementRef<typeof ReactNativeText>, TextProps>(
+  ({ children, className,  ...props }, ref) => {
+    return (
+      <ReactNativeText
+        ref={ref}
+        className={cn(className)}
+        {...props}
+      >
+        {children}
+      </ReactNativeText>
+    );
+  }
+)
+
+Text.displayName = "Text";
+
+
+
