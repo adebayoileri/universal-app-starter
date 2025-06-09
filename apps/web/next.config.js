79d6c7627e7abbcee7f416a29217220d1fdd39fe
Leon·Lan <irez@qq.com>
2025-06-09 09:52:47 +0800

    config origins

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,17 @@ const { withExpo } = require("@expo/next-adapter");
 
 module.exports = withExpo({
   reactStrictMode: true,
+  // 允许开发环境跨域访问
+  allowedDevOrigins: [
+    "192.168.0.15",
+    // 添加更多需要允许的IP地址
+    "192.168.1.0/24", // 允许整个192.168.1.x网段
+    "192.168.0.0/24", // 允许整个192.168.0.x网段
+    "10.0.0.0/24",    // 允许10.0.0.x网段
+    "127.0.0.1",
+    "172.16.0.0/12",
+    "172.16.1.0/12",
+  ],
   transpilePackages: [
     // you need to list `react-native` because `react-native-web` is aliased to `react-native`.
     "react-native",
